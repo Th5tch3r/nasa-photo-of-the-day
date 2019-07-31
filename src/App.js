db9887028547db74e5797803e4f6da08d7f802ec
Th5tch3r <amber.phan27@gmail.com>
2019-07-30 22:01:03 -0400

    starting on APOD

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
 
+import Welcome from "./components/welcome";
+
+
+
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      < Welcome />
+      <p className="rocket"> 🚀</p>
     </div>
   );
 }
---
+++ src/components/welcome.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const welcome = () => {
+    return (
+        <h1>Astronomy Photo Of Today! </h1>
+    );
+};
+
+export default welcome; 
